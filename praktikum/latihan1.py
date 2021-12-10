ab0e77a149096c68096350c6c0635008301c86e2
raissaputra <raissaputra02@gmail.com>
2021-12-10 21:04:01 +0700

    tambah code

--- praktikum/latihan1.py
+++ praktikum/latihan1.py
@@ -1,14 +1,30 @@
-dftrKontak = {'Ari': 6281267888, 'Dina': 6287677776}
+dftrKontak = {'Ari': '081267888', 'Dina': '087677776'}
 print('Nama\t| Nomor Telpon')
 print('='*22)
 for key, value in dftrKontak.items():
     print(key, '\t|', value)
 
+# tampilkan kontak ari
 print('\nKontaknya Ari : ', dftrKontak['Ari'])
+print()
+# Tambah kontak baru dengan nama Riko, nomor 087654544
 dftrKontak['Riko'] = 6287654544
+# Ubah kontak Dina dengan nomor baru 088999776
 dftrKontak['Dina'] = 6288999776
+# Tampilkan semua Nama
+print('Tampilkan semua Nama')
 print(dftrKontak.keys())
+print()
+# Tampilkan semua Nomor
+print('Tampilkan semua Nomor')
 print(dftrKontak.values())
+print()
+# Tampilkan daftar Nama dan nomornya
+print('Tampilkan daftar Nama dan nomornya')
 print(dftrKontak.items())
+print()
+# hapus kontak dina
 del dftrKontak['Dina']
-print('\n', dftrKontak.items())
+# tampilkan kontak setelah dina dihapus
+print('tampilkan kontak setelah dina dihapus')
+print(dftrKontak.items())
